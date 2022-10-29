4741cec7cccc11a02b3db7329bf20dcb8bdb461e
Abhinav Kumar <abhisri1997@gmail.com>
2022-10-29 14:50:14 +0530

    Time: 30 ms (86.78%), Space: 26.6 MB (12.53%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cs
+++ 0069-sqrtx/0069-sqrtx.cs
@@ -1,5 +1,18 @@
 public class Solution {
+    public int BinarySearch(int start, int end,int target)
+    {
+      if(target == 0) return 0;
+      if(target == 1) return 1;
+      while (start <= end)
+      {
+        int mid = start + ((end - start) >> 1); // 84, 41, 20, 9
+        if(mid == target/mid) return mid;
+        else if(mid > target/mid) end = mid - 1; // 83, 40, 19
+        else start = mid + 1;
+      }
+      return start - 1;
+    }
     public int MySqrt(int x) {
-        return (int)Math.Sqrt(x);
+        return BinarySearch(0, x, x);
     }
 }
