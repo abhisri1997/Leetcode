2858678a94a00052ffad095f2753cb4ad3687489
Abhinav Kumar <abhisri1997@gmail.com>
2022-11-15 11:46:23 +0530

    Time: 116 ms (65.97%), Space: 36.7 MB (42.78%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cs
+++ 0020-valid-parentheses/0020-valid-parentheses.cs
@@ -1,26 +1,26 @@
 public class Solution {
     public bool IsValid(string A) {
-        var st = new Stack<char>();
-            int len = A.Length;
-            var balancedParentheses = new Dictionary<char, char>();
-            balancedParentheses.Add('{', '}'); balancedParentheses.Add('[', ']');
-            balancedParentheses.Add('(', ')');
+      var st = new Stack<char>();
+      int len = A.Length;
+      var balancedParentheses = new Dictionary<char, char>();
+      balancedParentheses.Add('{', '}'); balancedParentheses.Add('[', ']');
+      balancedParentheses.Add('(', ')');
 
-            foreach (char ch in A)
-            {
-                if (balancedParentheses.ContainsKey(ch))
-                {
-                    st.Push(ch);
-                }
-                else if (st.Count > 0 && balancedParentheses[st.Peek()] == ch)
-                {
-                    st.Pop();
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return st.Count == 0;
+      foreach (char ch in A)
+      {
+        if (balancedParentheses.ContainsKey(ch))
+        {
+          st.Push(ch);
+        }
+        else if (st.Count > 0 && balancedParentheses[st.Peek()] == ch)
+        {
+          st.Pop();
+        }
+        else
+        {
+          return false;
+        }
+      }
+      return st.Count == 0;
     }
 }
